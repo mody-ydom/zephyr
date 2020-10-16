b48a41d7514164d433f922ae49ae070a116e9ad2
moaz-png <moazmamoud2002@gmail.com>
2020-10-16 21:11:58 +0200

    images folder

--- themes/zephyr/front-end/src/components/component-growth-guide/index.js
+++ themes/zephyr/front-end/src/components/component-growth-guide/index.js
@@ -1,5 +1,22 @@
 import './index.html';
 
 export default (container = document) => {
+  console.log('22');
+  const title = document.querySelectorAll('.guide-list .title');
+  title.addEventListener('click', (e) => {
+    console.log('ee');
+    if (podcastBody.style.height) {
+      podcastBody.style.height = null;
+      podcastBody.classList.remove('active');
+      title.querySelector('.arrow').classList.remove('active');
+      card.classList.remove('active');
+    }
+    else {
+      podcastBody.classList.add('active');
+      podcastBody.style.height = podcastBody.scrollHeight + 'px';
+      title.querySelector('.arrow').classList.add('active');
+      card.classList.add('active');
+    }
+  });
 
 };
