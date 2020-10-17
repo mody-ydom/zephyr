c559d16c9d88f6fdf2ec1af9b88ad3d3590aabcb
hossam <hossam.hendawy173@gmail.com>
2020-10-17 15:59:16 +0200

    component-hero done

--- themes/zephyr/front-end/src/components/index.js
+++ themes/zephyr/front-end/src/components/index.js
@@ -2,3 +2,4 @@ export template_component from './template-component';
 export header_component from './header-component';
 export footer_component from './footer-component';
 export component_growth_guide from './component-growth-guide';
+export component_hero from './component-hero';
