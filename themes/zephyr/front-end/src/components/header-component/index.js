309d208af10c21c3d0827d769c9db283f3219dd3
moaz-png <moazmamoud2002@gmail.com>
2020-10-17 15:13:02 +0200

    component-growth-guide not completed yet

--- themes/zephyr/front-end/src/components/header-component/index.js
+++ themes/zephyr/front-end/src/components/header-component/index.js
@@ -1,7 +1,8 @@
 import './index.html';
 
 export default (container = document) => {
-  
+  let headerContent = container.querySelector('.header-content');
+  if (headerContent) return ;
   let burgerMenu = document.getElementsByClassName('menu'),
      crossMenu = document.getElementsByClassName('cross'),
      links = document.getElementsByClassName('links');
@@ -11,15 +12,15 @@ export default (container = document) => {
   console.log(crossMenu);
   console.log(links);
 
-  burgerMenu.addEventListener('click', function () {
-    burgerMenu.classList.add('active');
-    crossMenu.classList.add('active');
-    links.classList.add('active');
-  });
+  // burgerMenu.addEventListener('click', function () {
+  //   burgerMenu.classList.add('active');
+  //   crossMenu.classList.add('active');
+  //   links.classList.add('active');
+  // });
 
-  crossMenu.addEventListener('click', function () {
-    crossMenu.classList.remove('active');
-    burgerMenu.classList.add('active');
-    links.classList.remove('active');
-  });
+  // crossMenu.addEventListener('click', function () {
+  //   crossMenu.classList.remove('active');
+  //   burgerMenu.classList.add('active');
+  //   links.classList.remove('active');
+  // });
 };
