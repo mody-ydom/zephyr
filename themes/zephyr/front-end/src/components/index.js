61774ef6cf52438151d5e48809ff27215554a828
hossam <hossam.hendawy173@gmail.com>
2020-10-17 03:27:26 +0200

    footer done

--- themes/zephyr/front-end/src/components/index.js
+++ themes/zephyr/front-end/src/components/index.js
@@ -1,4 +1,4 @@
 export template_component from './template-component';
-export header from './header-component';
-
+export header_component from './header-component';
+export footer_component from './footer-component';
 export component_growth_guide from './component-growth-guide';
