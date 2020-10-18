a7a0e5aa0c794860513414a9e35c9c24c87e9135
Yasser <yasserzakaria1993@gmail.com>
2020-10-18 14:32:54 +0200

    Fix Burger Menu JS

--- themes/zephyr/front-end/src/components/header-component/index.js
+++ themes/zephyr/front-end/src/components/header-component/index.js
@@ -2,25 +2,23 @@ import './index.html';
 
 export default (container = document) => {
   let headerContent = container.querySelector('.header-content');
-  if (headerContent) return ;
-  let burgerMenu = document.getElementsByClassName('menu'),
-     crossMenu = document.getElementsByClassName('cross'),
-     links = document.getElementsByClassName('links');
+  if (!headerContent) return;
+  
+  let burgerMenu = container.querySelector('.menu'),
+     crossMenu = container.querySelector('.cross'),
+     links = container.querySelector('.links');
    
   if (!burgerMenu) return;
-  console.log(burgerMenu);
-  console.log(crossMenu);
-  console.log(links);
 
-  // burgerMenu.addEventListener('click', function () {
-  //   burgerMenu.classList.add('active');
-  //   crossMenu.classList.add('active');
-  //   links.classList.add('active');
-  // });
+  burgerMenu.addEventListener('click', function () {
+    burgerMenu.classList.add('active');
+    crossMenu.classList.add('active');
+    links.classList.add('active');
+  });
 
-  // crossMenu.addEventListener('click', function () {
-  //   crossMenu.classList.remove('active');
-  //   burgerMenu.classList.add('active');
-  //   links.classList.remove('active');
-  // });
+  crossMenu.addEventListener('click', function () {
+    crossMenu.classList.remove('active');
+    burgerMenu.classList.remove('active');
+    links.classList.remove('active');
+  });
 };
