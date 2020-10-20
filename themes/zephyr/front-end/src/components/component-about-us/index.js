e629e706d938317baaafeb38611bd83738a5a4e1
moaz-png <moazmamoud2002@gmail.com>
2020-10-20 02:01:45 +0200

    some changes in swiper

--- themes/zephyr/front-end/src/components/component-about-us/index.js
+++ themes/zephyr/front-end/src/components/component-about-us/index.js
@@ -6,31 +6,52 @@ export default (container = document) => {
   let aboutUs = container.querySelector('.component-about-us');
   if (!aboutUs) return;
   Swiper.use([Pagination]);
-  let swiper = new Swiper('.about-us-content', {
-    slidesPerView: 1,
-    slidesPerColumn: 1,
-    slidesPerColumnFill: "row",
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      576:{
-        slidesPerView:2,
-        slidesPerColumn:2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        slidesPerColumn: 2,
-        spaceBetween: 76,
-        noSwiping: true,
-        allowTouchMove: false,
-      },
+  
+  let swiper = Swiper;
+  let init = false;
+  
+  function swiperMode() {
+    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 1024px)');
+    let desktop = window.matchMedia('(min-width: 1024px)');
+    
+    // Enable (for mobile)
+    if(mobile.matches) {
+      if (!init) {
+        init = true;
+        swiper = new Swiper('.about-us-content', {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          breakpoints: {
+            576:{
+              slidesPerView:2,
+              spaceBetween: 45,
+            }
+          }
+        });
+      }
     }
+    
+    // Disable (for desktop)
+    else if(desktop.matches) {
+      swiper.destroy();
+      init = false;
+    }
+  }
+  
+  /* On Load
+   **************************************************************/
+  window.addEventListener('load', function() {
+    swiperMode();
   });
   
-
+  /* On Resize
+   **************************************************************/
+  window.addEventListener('resize', function() {
+    swiperMode();
+  });
   
 };
