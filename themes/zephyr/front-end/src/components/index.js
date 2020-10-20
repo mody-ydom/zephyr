8958d57033594d783fce85b90ef4ae99a5684bbe
hossam <hossam.hendawy173@gmail.com>
2020-10-20 23:03:27 +0200

    component-our-process done

--- themes/zephyr/front-end/src/components/index.js
+++ themes/zephyr/front-end/src/components/index.js
@@ -15,4 +15,4 @@ export component_increase from './component-increase';
 export component_grid_images from './component-grid-images';
 export component_cards from './component-cards';
 export component_success_stories from './component-success-stories';
-export component_our_testimonials from './component-our-testimonials';
+export component_our_testimonials from './component-our-process';
