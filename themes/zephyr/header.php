7bddeb93cad17d7eaeabc7a7085d040f6f5373a0
Moemen Gaber <moemen.gaber.2@gmail.com>
2020-10-21 01:34:16 +0200

    fixed animation & font issues

--- themes/zephyr/header.php
+++ themes/zephyr/header.php
@@ -9,48 +9,46 @@
 	<?php wp_head(); ?>
   <link rel="stylesheet" href="https://use.typekit.net/cca7lwo.css">
 </head>
-<?php
-	$header_logo = get_field( 'header_logo', 'options' );
-	$header_type = get_field( 'colored_header' );
-?>
+
 <body <?php body_class(); ?> data-barba="wrapper">
   <div class="barba-overlay-transition"></div>
 	<?php wp_body_open(); ?>
-  <div smooth-scroll-container="">
-      <header>
-          <div class="container">
-              <div class="header-content">
-                  <!--      burger menu and cross-->
-                  <div class="menu-and-cross">
-                      <div class="menu">
-                          <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <line x1="28" y1="0.75" x2="-6.79088e-08" y2="0.749997" stroke="#fefefd" stroke-width="1.5"/>
-                              <line x1="28" y1="9.75" x2="-6.7828e-08" y2="9.75" stroke="#fefefd" stroke-width="1.5"/>
-                              <line x1="28" y1="18.75" x2="-6.7828e-08" y2="18.75" stroke="#fefefd" stroke-width="1.5"/>
-                          </svg>
-                      </div>
-                      <div class="cross">
-                          <svg  width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <line x1="1.46967" y1="19.1474" x2="19.1473" y2="1.46974" stroke="#fefefd" stroke-width="1.5"/>
-                              <line x1="2.53033" y1="1.46967" x2="20.208" y2="19.1473" stroke="#fefefd" stroke-width="1.5"/>
-                          </svg>
-                      </div>
+  <header>
+      <div class="container">
+          <div class="header-content">
+              <!--      burger menu and cross-->
+              <div class="menu-and-cross">
+                  <div class="menu">
+                      <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <line x1="28" y1="0.75" x2="-6.79088e-08" y2="0.749997" stroke="#fefefd" stroke-width="1.5"/>
+                          <line x1="28" y1="9.75" x2="-6.7828e-08" y2="9.75" stroke="#fefefd" stroke-width="1.5"/>
+                          <line x1="28" y1="18.75" x2="-6.7828e-08" y2="18.75" stroke="#fefefd" stroke-width="1.5"/>
+                      </svg>
                   </div>
-                  <!--      logo-->
-                  <a class="logo light" href="#">
-                      <img src="../../html/images/logo.png" alt="logo">
-                  </a>
-                  <a class="logo dark" href="#">
-                      <img src="../../html/images/gray-logo.png" alt="logo">
-                  </a>
-                  <!--    links-->
-                  <div class="links">
-                      <a class="small-link" href="#"><span data-hover="Sector Expertise">Sector Expertise</span></a>
-                      <a class="small-link" href="#"><span data-hover="About">About</span></a>
-                      <a class="small-link" href="#"><span data-hover="Growth Guide">Growth Guide</span></a>
-                      <a class="small-link" href="#"><span data-hover="Say hello">Say hello</span></a>
+                  <div class="cross">
+                      <svg  width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <line x1="1.46967" y1="19.1474" x2="19.1473" y2="1.46974" stroke="#fefefd" stroke-width="1.5"/>
+                          <line x1="2.53033" y1="1.46967" x2="20.208" y2="19.1473" stroke="#fefefd" stroke-width="1.5"/>
+                      </svg>
                   </div>
               </div>
+              <!--      logo-->
+              <a class="logo light" href="#">
+                  <img src="../../html/images/logo.png" alt="logo">
+              </a>
+              <a class="logo dark" href="#">
+                  <img src="../../html/images/gray-logo.png" alt="logo">
+              </a>
+              <!--    links-->
+              <div class="links">
+                  <a class="small-link" href="#"><span data-hover="Sector Expertise">Sector Expertise</span></a>
+                  <a class="small-link" href="#"><span data-hover="About">About</span></a>
+                  <a class="small-link" href="#"><span data-hover="Growth Guide">Growth Guide</span></a>
+                  <a class="small-link" href="#"><span data-hover="Say hello">Say hello</span></a>
+              </div>
           </div>
-      </header>
-  </div>
+      </div>
+  </header>
+  <div class="smooth-scroller" smooth-scroll-container>
+    <main <?=$header_type || is_404() ? 'colored' : ''?> data-barba="container" data-barba-namespace="home"><!-- overlay dark menu -->
+      <div class="wrapper-page-transition"></div>
