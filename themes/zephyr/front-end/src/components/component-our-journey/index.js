718b38801465d57cab78741b873ab6ac830ca166
moaz-png <moazmamoud2002@gmail.com>
2020-10-20 03:59:06 +0200

    added swiper-pagination style in general

--- themes/zephyr/front-end/src/components/component-our-journey/index.js
+++ themes/zephyr/front-end/src/components/component-our-journey/index.js
@@ -1,5 +1,6 @@
 import './index.html';
 import Swiper, {Pagination} from 'swiper';
+import 'swiper/swiper.scss';
 
 export default (container = document) => {
   let ourJourney = container.querySelector('.component-our-journey');
@@ -7,45 +8,18 @@ export default (container = document) => {
   Swiper.use([Pagination]);
   let swiper2 = Swiper;
   let init = false;
-  swiper2 = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    slidesPerColumn: 1,
-    slidesPerColumnFill: "row",
-    spaceBetween: 30,
-    noSwiping: true,
-    allowTouchMove: false,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      576:{
-        slidesPerView:2,
-        slidesPerColumn:3,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 3,
-        slidesPerColumn: 3,
-        spaceBetween: 40
-      },
-    }
-  });
   
   function swiperMode() {
-    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
-    let tablet = window.matchMedia('(min-width: 769px) and (max-width: 1024px)');
+    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 1024px)');
     let desktop = window.matchMedia('(min-width: 1024px)');
     
     // Enable (for mobile)
     if(mobile.matches) {
       if (!init) {
         init = true;
-        swiper2 = new Swiper('.swiper-container', {
-          slidesPerView: 1,
-          slidesPerColumn: 1,
-          slidesPerColumnFill: "row",
-          spaceBetween: 30,
+        swiper2 = new Swiper('.our-journey-content', {
+          slidesPerView: 2,
+          spaceBetween: 50,
           pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -53,37 +27,23 @@ export default (container = document) => {
           breakpoints: {
             576:{
               slidesPerView:2,
-              slidesPerColumn:1,
               spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 3,
-              slidesPerColumn: 2,
-              spaceBetween: 76
-            },
+            }
           }
         });
       }
-      
     }
-    
-    // Disable (for tablet)
-    else if(tablet.matches) {
-      swiper.destroy();
-      init = false;
-    }
-    
     // Disable (for desktop)
     else if(desktop.matches) {
-      swiper.destroy();
+      swiper2.destroy();
       init = false;
     }
   }
   
-  // window.addEventListener('load',function (){
-  //   swiperMode();
-  // });
-  // window.addEventListener('resize', function() {
-  //   swiperMode();
-  // });
+  window.addEventListener('load',function (){
+    swiperMode();
+  });
+  window.addEventListener('resize', function() {
+    swiperMode();
+  });
 };
