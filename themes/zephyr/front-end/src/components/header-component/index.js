6f12597fc19c285b0d9c779d9e740debe69d356e
moaz-png <moazmamoud2002@gmail.com>
2020-10-21 17:51:54 +0200

    header on scroll change background not working with smooth scroll

--- themes/zephyr/front-end/src/components/header-component/index.js
+++ themes/zephyr/front-end/src/components/header-component/index.js
@@ -5,20 +5,29 @@ export default (container = document) => {
   if (!headerContent) return;
   
   let burgerMenu = container.querySelector('.menu'),
-     crossMenu = container.querySelector('.cross'),
-     links = container.querySelector('.links');
-   
+    crossMenu = container.querySelector('.cross'),
+    links = container.querySelector('.links');
   if (!burgerMenu) return;
-
+  
   burgerMenu.addEventListener('click', function () {
     burgerMenu.classList.add('active');
     crossMenu.classList.add('active');
     links.classList.add('active');
   });
-
+  
   crossMenu.addEventListener('click', function () {
     crossMenu.classList.remove('active');
     burgerMenu.classList.remove('active');
     links.classList.remove('active');
+  });
+  
+  window.addEventListener('scroll', function () {
+    if (this.scrollY >= 100) {
+      console.log('>200');
+      headerContent.parentElement.parentElement.classList.add('active');
+    }
+    else {
+      headerContent.parentElement.parentElement.classList.remove('active');
+    }
   });
 };
