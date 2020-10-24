cd5778ce3b742d6d9f6135e7fdcffd01012fe8e0
Moemen Gaber <moemen.gaber.2@gmail.com>
2020-10-24 08:22:47 +0200

    Added Component-our-testimonials & fixed issues in component-home-swiper

--- themes/zephyr/functions.php
+++ themes/zephyr/functions.php
@@ -281,6 +281,24 @@ function register_acf_block_types()
             ]
         ]
     ));
+
+    acf_register_block_type(array(
+        'name' => 'component-our-testimonials',
+        'title' => __('Component Our Testimonials'),
+        'render_template' => 'template-parts/blocks/component-our-testimonials/index.php',
+        'enqueue_style' => $developing ? '' : get_template_directory_uri() . '/template-parts/blocks/component-our-testimonials/style.css',
+        'category' => 'zephyr-home',
+        'icon' => 'admin-appearance',
+        'mode' => 'preview',
+        'example' => [
+            'attributes' => [
+                'mode' => 'preview',
+                'data' => [
+                    'is_screenshot' => true,
+                ],
+            ]
+        ]
+    ));
 }
 
 // Check if function exists and hook into setup.
