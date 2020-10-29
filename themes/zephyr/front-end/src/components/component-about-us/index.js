8112d6c3c9445be42216cc9831a3d3cd4a416ddc
ModyydoM <modyydom@gmail.com>
2020-10-29 23:13:38 +0200

    sticky header
    Signed-off-by: ModyydoM <modyydom@gmail.com>

--- themes/zephyr/front-end/src/components/component-about-us/index.js
+++ themes/zephyr/front-end/src/components/component-about-us/index.js
@@ -13,45 +13,43 @@ export default (container = document) => {
   function swiperMode() {
     let mobile = window.matchMedia('(max-width: 1023.98px)');
     let desktop = window.matchMedia('(min-width: 1024px)');
-    
+  
     // Enable (for mobile)
-    if(mobile.matches) {
-      if (!init) {
-        init = true;
-        swiper = new Swiper('.about-us-content', {
-          slidesPerView: 1,
-          spaceBetween: 20,
-          grabCursor: true,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+    if (!init && mobile.matches) {
+      init = true;
+      swiper = new Swiper('.about-us-content', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        grabCursor: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 45,
           },
-          breakpoints: {
-            576:{
-              slidesPerView:2,
-              spaceBetween: 45,
-            }
-          }
-        });
-      }
+        },
+      });
     }
-    
+  
     // Disable (for desktop)
-    else if(desktop.matches) {
-      swiper.destroy();
+    else if (init && desktop.matches) {
+      swiper?.destroy();
       init = false;
     }
   }
   
   /* On Load
    **************************************************************/
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     swiperMode();
   });
   
   /* On Resize
    **************************************************************/
-  window.addEventListener('resize', function() {
+  window.addEventListener('resize', function () {
     swiperMode();
   });
   
