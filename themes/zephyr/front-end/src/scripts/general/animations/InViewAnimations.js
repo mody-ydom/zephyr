cf1d249c176524ab2977739e8d6bc0dac3f3d1be
ModyydoM <modyydom@gmail.com>
2020-10-30 04:40:52 +0200

    fixing mobile menu style
    Signed-off-by: ModyydoM <modyydom@gmail.com>

--- themes/zephyr/front-end/src/scripts/general/animations/InViewAnimations.js
+++ themes/zephyr/front-end/src/scripts/general/animations/InViewAnimations.js
@@ -39,6 +39,7 @@ export default function inViewAnimations(container = document) {
   gsap.set(elements2, {autoAlpha: 0});
   ScrollTrigger.batch(elements2, {
     onEnter: batch => {
+      console.log(batch);
       gsap.set(batch, {autoAlpha: 1});
       gsap.fromTo(batch, {autoAlpha:0,yPercent: (_, target) => 50 * (target.classList.contains('iv-st-from-bottom') ? 1 : -1)},{
         duration: .7,
@@ -63,67 +64,8 @@ export default function inViewAnimations(container = document) {
     start: 'top 80%',
     once: true,
   });
-  const elements3 = getElementsForAnimation(container, '.iv-st-from-bottom-stories');
-  gsap.set(elements3, {autoAlpha: 0});
-  ScrollTrigger.batch(elements3, {
-    onEnter: batch => {
-      gsap.set(batch, {autoAlpha: 1});
-      gsap.fromTo(batch, {autoAlpha:0,y: 300},{
-        duration: .6,
-        autoAlpha: 0,
-        y: 0,
-        ease: 'power2.out',
-        stagger: .6,
-        clearProps: 'transform',
-      });
-    },
-    onEnterBack: batch => {
-      gsap.set(batch, {autoAlpha: 1});
-      gsap.fromTo(batch, {autoAlpha:0,y: 300},{
-        duration: .6,
-        autoAlpha: 0,
-        y: 0,
-        ease: 'power2.out',
-        stagger: .6,
-        clearProps: 'transform',
-      });
-    },
-    start: 'top 80%',
-    once: true,
-    // markers:true
-  });
-  const elements4 = getElementsForAnimation(container, '.iv-st-from-bottom-team-names');
-  gsap.set(elements4, {autoAlpha: 0});
-  ScrollTrigger.batch(elements4, {
-    onEnter: batch => {
-      gsap.set(batch, {autoAlpha: 1});
-      gsap.fromTo(batch, {autoAlpha:0,yPercent: 50},{
-        duration: .7,
-        autoAlpha: 0,
-        yPercent: 0,
-        ease: 'power1.out',
-        stagger: .1,
-        clearProps: 'transform',
-      });
-    },
-    onEnterBack: batch => {
-      gsap.set(batch, {autoAlpha: 1});
-      gsap.fromTo(batch, {autoAlpha:0,y: 50},{
-        duration: .7,
-        autoAlpha: 0,
-        y: 0,
-        ease: 'power1.out',
-        stagger: .1,
-        clearProps: 'transform',
-      });
-    },
-    start: 'top 95%',
-    once: true,
-    // markers:true
-  });
+  
+  ScrollTrigger.addEventListener('refreshInit', () => gsap.set('.iv-st-from-top, .iv-st-from-bottom', {yPercent: 0}));
 }
-
-ScrollTrigger.addEventListener('refreshInit', () => gsap.set('.iv-st-from-bottom-stories', {y: 0}));
-ScrollTrigger.addEventListener('refreshInit', () => gsap.set('.iv-st-from-top, .iv-st-from-bottom, .iv-st-from-bottom-team-names', {yPercent: 0}));
 
 
