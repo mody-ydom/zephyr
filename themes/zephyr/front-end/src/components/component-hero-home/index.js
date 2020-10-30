cfb0ac6fde02689da6cbf329c6d6210c63d55f2a
ModyydoM <modyydom@gmail.com>
2020-10-30 02:03:35 +0200

    home hero animations DONE
    Signed-off-by: ModyydoM <modyydom@gmail.com>

--- themes/zephyr/front-end/src/components/component-hero-home/index.js
+++ themes/zephyr/front-end/src/components/component-hero-home/index.js
@@ -15,8 +15,8 @@ export default (container = document) => {
       once: true,
     },
   })
-    .fromTo(hero.querySelectorAll('.test-blob,.circle.oval'), {autoAlpha: 0, x: 100}, {autoAlpha: 1, x: 0, duration: 0.75, stagger: 0.4})
-    .fromTo(hero.querySelectorAll('.circle.orange-circle'), {autoAlpha: 0, y: 100}, {autoAlpha: 1, y: 0, duration: 0.5, stagger: 0.2}, '<+=.3')
-    .fromTo(hero.querySelectorAll('.circle.aqua-circle'), {autoAlpha: 0, y: 100}, {autoAlpha: 1, y: 0, duration: 0.5, stagger: 0.2}, '<+=.3')
-    .fromTo(hero.querySelectorAll('.circle.dark-circle'), {autoAlpha: 0, y: -100}, {autoAlpha: 1, y: 0, duration: 0.5, stagger: 0.2}, '<+=.3');
+    .fromTo(hero.querySelectorAll('.test-blob,.circle.oval'), {autoAlpha: 0, xPercent: 100}, {autoAlpha: 1, xPercent: 0, duration: 1, stagger: 0.4})
+    .fromTo(hero.querySelectorAll('.circle.orange-circle'), {autoAlpha: 0, y: 100}, {autoAlpha: 1, y: 0, duration: 0.8, stagger: 0.2}, '<+=.4')
+    .fromTo(hero.querySelectorAll('.circle.aqua-circle'), {autoAlpha: 0, y: 100}, {autoAlpha: 1, y: 0, duration: 0.8, stagger: 0.2}, '<+=.4')
+    .fromTo(hero.querySelectorAll('.circle.dark-circle'), {autoAlpha: 0, y: -100}, {autoAlpha: 1, y: 0, duration: 0.8, stagger: 0.2}, '<+=.4');
 };
