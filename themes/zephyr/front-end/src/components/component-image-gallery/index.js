9f80998191e6b7c25c4358ccf06ffe4c0583af84
Yasser <yasserzakaria1993@gmail.com>
2020-10-30 07:31:04 +0200

    single style

--- themes/zephyr/front-end/src/components/component-image-gallery/index.js
+++ themes/zephyr/front-end/src/components/component-image-gallery/index.js
@@ -14,37 +14,38 @@ export default (container = document) => {
   
   resizeAll(sections);
   
-  const imageWrappers = getElementsForAnimation(container, '.component-image-gallery .image-wrapper');
-  for (let imageWrapper of imageWrappers) {
-    
-    const background = imageWrapper.querySelector('img.bg');
-    const leftImage = imageWrapper.querySelector('img.left');
-    const rightImage = imageWrapper.querySelector('img.right');
-    const tlOptions = isAdmin ? {} : {
-      scrollTrigger: {
-        trigger: imageWrapper,
-        start: `top ${imageWrapper.classList.contains('pt-0') ? '50' : '80'}%`,
-        once: true,
-      },
-    };
-    const imageWrapperTl = gsap.timeline(tlOptions);
-    if (imageWrapper.classList.contains('full-width')) {
-      imageWrapperTl
-        .add(gsap.fromTo(leftImage, {x: '+=50'}, {x: '-=50', duration: 1}), 0)
-        .add(gsap.fromTo(rightImage, {x: '-=50'}, {x: '+=50', duration: 1}), 0);
-    }
-    else {
-      if (leftImage) {
-        imageWrapperTl.add(gsap.fromTo(leftImage, {y: '+=50'}, {y: '-=50', duration: 1}), 0);
-      }
-      else {
-        imageWrapperTl.add(gsap.fromTo(background, {scale: 1.25}, {scale: 1, duration: 1}), 0);
-      }
-    }
-  }
-  
   
   function resizeAll(sections) {
+  
+    const imageWrappers = getElementsForAnimation(container, '.component-image-gallery .image-wrapper');
+    for (let imageWrapper of imageWrappers) {
+    
+      const background = imageWrapper.querySelector('img.bg');
+      const leftImage = imageWrapper.querySelector('img.left');
+      const rightImage = imageWrapper.querySelector('img.right');
+      const tlOptions = isAdmin ? {} : {
+        scrollTrigger: {
+          trigger: imageWrapper,
+          start: `top ${imageWrapper.classList.contains('pt-0') ? '50' : '80'}%`,
+          once: true,
+        },
+      };
+      const imageWrapperTl = gsap.timeline(tlOptions);
+      if (imageWrapper.classList.contains('full-width')) {
+        imageWrapperTl
+          .add(gsap.fromTo(leftImage, {x: '+=50'}, {x: '-=50', duration: 1}), 0)
+          .add(gsap.fromTo(rightImage, {x: '-=50'}, {x: '+=50', duration: 1}), 0);
+      }
+      else {
+        if (leftImage) {
+          imageWrapperTl.add(gsap.fromTo(leftImage, {y: '+=50'}, {y: '-=50', duration: 1}), 0);
+        }
+        else {
+          imageWrapperTl.add(gsap.fromTo(background, {scale: 1.25}, {scale: 1, duration: 1}), 0);
+        }
+      }
+    }
+  
     for (let section of sections) {
       const grid = section.querySelector('.images-grid');
   
@@ -70,14 +71,15 @@ export default (container = document) => {
   const images = getElementsForAnimation(container, '.component-image-gallery img');
   
   for (let image of images) {
-    console.log(image,image.closest('.grid-item'));
     if (image.complete) {
-      resizeGridItem(image.closest('.grid-item'));
+      resizeAll(sections);
+      // resizeGridItem(image.closest('.grid-item'));
       // ScrollTrigger.refresh();
     }
     else {
       image.addEventListener('load', () => {
-        resizeGridItem(image.closest('.grid-item'));
+        resizeAll(sections);
+        // resizeGridItem(image.closest('.grid-item'));
         // ScrollTrigger.refresh();
       });
     }
