16156eee1f0720eb32b322fb065e77c5374585c7
Yasser <yasserzakaria1993@gmail.com>
2020-10-30 07:43:21 +0200

    single style

--- themes/zephyr/front-end/src/components/component-image-gallery/index.js
+++ themes/zephyr/front-end/src/components/component-image-gallery/index.js
@@ -15,36 +15,36 @@ export default (container = document) => {
   resizeAll(sections);
   
   
-  function resizeAll(sections) {
-  
-    const imageWrappers = getElementsForAnimation(container, '.component-image-gallery .image-wrapper');
-    for (let imageWrapper of imageWrappers) {
+  const imageWrappers = getElementsForAnimation(container, '.component-image-gallery .image-wrapper');
+  for (let imageWrapper of imageWrappers) {
     
-      const background = imageWrapper.querySelector('img.bg');
-      const leftImage = imageWrapper.querySelector('img.left');
-      const rightImage = imageWrapper.querySelector('img.right');
-      const tlOptions = isAdmin ? {} : {
-        scrollTrigger: {
-          trigger: imageWrapper,
-          start: `top ${imageWrapper.classList.contains('pt-0') ? '50' : '80'}%`,
-          once: true,
-        },
-      };
-      const imageWrapperTl = gsap.timeline(tlOptions);
-      if (imageWrapper.classList.contains('full-width')) {
-        imageWrapperTl
-          .add(gsap.fromTo(leftImage, {x: '+=50'}, {x: '-=50', duration: 1}), 0)
-          .add(gsap.fromTo(rightImage, {x: '-=50'}, {x: '+=50', duration: 1}), 0);
+    const background = imageWrapper.querySelector('img.bg');
+    const leftImage = imageWrapper.querySelector('img.left');
+    const rightImage = imageWrapper.querySelector('img.right');
+    const tlOptions = isAdmin ? {} : {
+      scrollTrigger: {
+        trigger: imageWrapper,
+        start: `top ${imageWrapper.classList.contains('pt-0') ? '50' : '80'}%`,
+        once: true,
+      },
+    };
+    const imageWrapperTl = gsap.timeline(tlOptions);
+    if (imageWrapper.classList.contains('full-width')) {
+      imageWrapperTl
+        .add(gsap.fromTo(leftImage, {yPercent:-50, x: '+=50'}, {x: '-=50', duration: 1}), 0)
+        .add(gsap.fromTo(rightImage, {yPercent:-50, x: '-=50'}, {x: '+=50', duration: 1}), 0);
+    }
+    else {
+      if (leftImage) {
+        imageWrapperTl.add(gsap.fromTo(leftImage, {yPercent:-50,xPercent:-50,y: '+=50'}, {y: '-=50', duration: 1}), 0);
       }
       else {
-        if (leftImage) {
-          imageWrapperTl.add(gsap.fromTo(leftImage, {y: '+=50'}, {y: '-=50', duration: 1}), 0);
-        }
-        else {
-          imageWrapperTl.add(gsap.fromTo(background, {scale: 1.25}, {scale: 1, duration: 1}), 0);
-        }
+        imageWrapperTl.add(gsap.fromTo(background, {scale: 1.25}, {scale: 1, duration: 1}), 0);
       }
     }
+  }
+  function resizeAll(sections) {
+  
   
     for (let section of sections) {
       const grid = section.querySelector('.images-grid');
