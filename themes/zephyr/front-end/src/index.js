6af73a1a6611622c26f31ba149a6dcc4f10895fc
Yasser <yasserzakaria1993@gmail.com>
2020-11-05 19:12:37 +0200

    Fianlly

--- themes/zephyr/front-end/src/index.js
+++ themes/zephyr/front-end/src/index.js
@@ -23,9 +23,7 @@ const reInvokableFunction = (container = document) => {
   const bodyScrollBar = Scrollbar.get(document.querySelector('[smooth-scroll-container]'));
   bodyScrollBar.updatePluginOptions('dampScroll', {amount: 0});
   document.querySelector('header').classList.remove('freeze');
-  setTimeout(()=>{
-    bodyScrollBar.scrollTo(0, 0, 500);
-  },500)
+  bodyScrollBar.scrollTo(0, 0, 500);
   bodyScrollBar.update();
   for (let scrollTriggerInstance of ScrollTrigger.getAll()) {
     scrollTriggerInstance.kill();
