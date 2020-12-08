890c62bd325cf11407af88bea98ecd68dd0a4910
Yasser <yasserzakaria1993@gmail.com>
2020-12-08 17:17:52 +0200

    text color from black to gray

--- themes/zephyr/front-end/src/scripts/functions/prevent_allowPageScroll.js
+++ themes/zephyr/front-end/src/scripts/functions/prevent_allowPageScroll.js
@@ -1,18 +1,18 @@
 export function allowPageScroll(element = window) {
-  element.removeEventListener && element.removeEventListener('DOMMouseScroll', preventScrollDefault, false);
-  element.removeEventListener('wheel', preventScrollDefault);
-  element.removeEventListener('mousewheel', preventScrollDefault);
-  element.removeEventListener('touchmove', preventScrollDefault);
-  element.removeEventListener('keydown', preventKeydownDefault);
+  element?.removeEventListener && element?.removeEventListener('DOMMouseScroll', preventScrollDefault, false);
+  element?.removeEventListener('wheel', preventScrollDefault);
+  element?.removeEventListener('mousewheel', preventScrollDefault);
+  element?.removeEventListener('touchmove', preventScrollDefault);
+  element?.removeEventListener('keydown', preventKeydownDefault);
   document.body.removeEventListener('touchmove', preventScrollDefault);
 }
 
 export function preventPageScroll(element = window) {
-  element.addEventListener && element.addEventListener('DOMMouseScroll', preventScrollDefault, false);
-  element.addEventListener('wheel', preventScrollDefault, {passive: false});
-  element.addEventListener('mousewheel', preventScrollDefault, {passive: false});
-  element.addEventListener('touchmove', preventScrollDefault, {passive: false});
-  element.addEventListener('keydown', preventKeydownDefault, {passive: false});
+  element?.addEventListener && element?.addEventListener('DOMMouseScroll', preventScrollDefault, false);
+  element?.addEventListener('wheel', preventScrollDefault, {passive: false});
+  element?.addEventListener('mousewheel', preventScrollDefault, {passive: false});
+  element?.addEventListener('touchmove', preventScrollDefault, {passive: false});
+  element?.addEventListener('keydown', preventKeydownDefault, {passive: false});
   document.body.addEventListener('touchmove', preventScrollDefault, {passive: false});
 }
 
